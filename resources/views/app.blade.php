95672c5d289b1b07e2a176e35170e6b878c72214
Hemant J <hemant@procoders.in>
2024-06-30 15:55:31 +0530

    logo added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,6 +15,8 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
         crossorigin="anonymous"></script>
+    <link rel="icon" type="image/x-icon" href="/logo.png">
+
 
     <style>
         .material-symbols-outlined {
