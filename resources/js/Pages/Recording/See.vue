f33d7918047f62449cb31ed3703cabb549863d27
Hemant Jangid <rj.hemantjangid@gmail.com>
2024-07-16 19:12:38 +0530

    v

--- resources/js/Pages/Recording/See.vue
+++ resources/js/Pages/Recording/See.vue
@@ -24,6 +24,10 @@ import 'url-polyfill';
 import * as Sentry from '@sentry/browser';
 import Shr from 'shr-buttons';
 
+import Plyr from '../../../../public/plyr/js/plyr';
+console.log(Plyr)
+
+
 export default {
     components: {
         AuthenticatedLayout
@@ -31,7 +35,7 @@ export default {
     props : ['recording'],
     mounted(){
         const selector = '#player';
-        alert('sdsdsd')
+
         Shr.setup('.js-shr', {
         count: {
             className: 'button__count',
@@ -41,8 +45,104 @@ export default {
         },
         });
 
-        const player = new Plyr(selector)
-        console.log(player,'player')
+        const player = new Plyr(selector, {
+            debug: true,
+            title: 'View From A Blue Moon',
+            iconUrl: 'dist/demo.svg',
+            keyboard: {
+                global: true,
+            },
+            tooltips: {
+                controls: true,
+            },
+            captions: { active: true},
+            previewThumbnails: {
+                enabled: true,
+                src: [],
+            },
+        })
+
+        // Expose for tinkering in the console
+        window.player = player;
+
+        // Setup type toggle
+        const buttons = document.querySelectorAll('[data-source]');
+        const types = Object.keys(sources);
+        const historySupport = Boolean(window.history && window.history.pushState);
+        let currentType = window.location.hash.substring(1);
+        const hasInitialType = currentType.length;
+
+        function render(type) {
+            // Remove active classes
+            Array.from(buttons).forEach((button) => button.parentElement.classList.toggle('active', false));
+
+            // Set active on parent
+            document.querySelector(`[data-source="${type}"]`).classList.toggle('active', true);
+
+            // Show cite
+            Array.from(document.querySelectorAll('.plyr__cite')).forEach((cite) => {
+                // eslint-disable-next-line no-param-reassign
+                cite.hidden = true;
+            });
+
+            document.querySelector(`.plyr__cite--${type}`).hidden = false;
+        }
+
+        function setSource(type, init) {
+            // Bail if new type isn't known, it's the current type, or current type is empty (video is default) and new type is video
+            if (!types.includes(type) || (!init && type === currentType) || (!currentType.length && type === 'video')) {
+                return;
+            }
+
+            // Set the new source
+            player.source = sources[type];
+
+            // Set the current type for next time
+            currentType = type;
+
+            render(type);
+        }
+
+        Array.from(buttons).forEach((button) => {
+            button.addEventListener('click', () => {
+                const type = button.getAttribute('data-source');
+
+                setSource(type);
+
+                if (historySupport) {
+                window.history.pushState({ type }, '', `#${type}`);
+                }
+            });
+        });
+
+        window.addEventListener('popstate', (event) => {
+            if (event.state && Object.keys(event.state).includes('type')) {
+                setSource(event.state.type);
+            }
+        });
+
+        if (!hasInitialType) {
+            currentType = 'video';
+            }
+
+            // Replace current history state
+            if (historySupport && types.includes(currentType)) {
+            window.history.replaceState({ type: currentType }, '', hasInitialType ? `#${currentType}` : '');
+            }
+
+            // If it's not video, load the source
+            if (currentType !== 'video') {
+            setSource(currentType, true);
+            }
+
+            render(currentType);
+
+
+
+
+
+
+
     }
 }
 </script>
--- resources/views/recording/view.blade.php
+++ resources/views/recording/view.blade.php
@@ -1,5 +1,5 @@
 <script src="https://cdn.plyr.io/3.7.8/plyr.js"></script>
-    <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
+<link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
 <video id="player" playsinline controls data-poster="/path/to/poster.jpg">
     <source src="{{$recording->link}}" type="video/mp4" />
 </video>
